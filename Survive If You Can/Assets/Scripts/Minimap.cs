116109e8ad9860ab50ea9eff61b95cf46a81d8e7
Cristea Daniel <cristea3000@yahoo.com>
2023-07-23 23:37:47 +0300

    MiniMap added for better gaming experience

---
+++ Survive If You Can/Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = Player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+
+    }
+}
